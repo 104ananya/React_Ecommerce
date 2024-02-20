d2caf9de041b735db4891f606b0ef3dbae10916d
KUMARI ANANYA <75521183+104ananya@users.noreply.github.com>
2024-02-20 20:08:45 +0530

    Axios & calling API

--- src/context/productcontext.jsx
+++ src/context/productcontext.jsx
@@ -2,23 +2,45 @@
 // 2. Delivery Man
 // 3. Customer - useContext hook
 
+import axios from "axios";
 import { createContext, useContext } from "react";
+import { useEffect } from "react";
 
 // STEP 1
 const AppContext = createContext();
 
+// API url
+// const API = "https://api.pujakaitem.com/api/products";
+const API = "https://fakestoreapi.com/products";
+
+
+const getProduct = async (url) => {
+    const res = await axios.get(url);
+
+    console.log(res)
+    const products = res.data;
+};
+
+
 // STEP 2
-const AppProvider = ({children}) => {
-    return <AppContext.Provider value={{name: "Ananya"}}>
-        {children}
+const AppProvider = ({ children }) => {
+
+  //Function for API calling
+  useEffect(() => {
+    getProduct(API);
+  }, []);
+
+  return (
+    <AppContext.Provider value={{ name: "Ananya" }}>
+      {children}
     </AppContext.Provider>
-}
+  );
+};
 
 // custom hook
 const useProductContext = () => {
-    return useContext(AppContext);
-}
+  return useContext(AppContext);
+};
 
-
-export {AppProvider, AppContext, useProductContext};
+export { AppProvider, AppContext, useProductContext };
 // REMEMBER to wrap whole <App /> inside this Approvider
