5f9bba3cb961e98b503dee845f604f724cc61490
KUMARI ANANYA <75521183+104ananya@users.noreply.github.com>
2024-02-27 13:34:52 +0530

    Basic Layout of Products page

---
+++ src/components/SortingSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SortingSection = () => {
+  return (
+    <div className="flex justify-between mb-4">
+    <div>icon</div>
+    <div>Total</div>
+    <div>sort</div>
+  </div>
+  )
+}
+
+export default SortingSection
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,17 +1,38 @@
 import React from "react";
-import { useProductContext } from "../context/productcontext";
+import ProductList from "../components/ProductList";
+import FilterSection from "../components/FilterSection";
+import SortingSection from "../components/SortingSection";
 
 const Products = () => {
-  // const {name} = useContext(AppContext);
-  const {name} = useProductContext();
-
-
-
   return (
-    <div >
-      <p>Name: {name}</p>
-      <div>Products</div>
-    </div>
+    <main className="relative mt-[75px] padding-x">
+      {/* Categories */}
+      <div className="flex justify-between items-center bg-gray-200 p-4 mb-4">
+        <span>Category 1</span>
+        <span>Category 2</span>
+        <span>Category 3</span>
+        <span>Category 3</span>
+        <span>Category 3</span>
+      </div>
+
+      {/* Product grid */}
+      <div className="flex">
+        {/* 1st column - 1/4 width */}
+        <div className="w-1/4 bg-gray-200">
+          <FilterSection />
+        </div>
+
+        {/* 2nd column - 3/4 width */}
+        <div className="w-3/4 bg-slate-600">
+          <SortingSection />
+
+
+          <div className="flex justify-between">
+            <ProductList />
+          </div>
+        </div>
+      </div>
+    </main>
   );
 };
 
