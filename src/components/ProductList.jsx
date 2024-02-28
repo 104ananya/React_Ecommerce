d496e7fcfd1bec856c842fca98bf3f371cb22376
KUMARI ANANYA <75521183+104ananya@users.noreply.github.com>
2024-02-28 09:23:05 +0530

    conditional rendering -- list/grid view

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useFilterContext } from '../context/filterContext'
+import GridView from './GridView';
+import ListView from './ListView';
 
 const ProductList = () => {
-  return (
-    <div>ProductList</div>
-  )
+
+  const {filter_products, list_view} = useFilterContext();
+
+  if(list_view === true){
+    return <ListView products = {filter_products}/>
+  }
+
+  if(list_view === false){
+    return <GridView products = {filter_products} />
+  }
 }
 
 export default ProductList
