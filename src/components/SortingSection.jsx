cf770597814e2c00913d3f440b633900a0dd3b1c
KUMARI ANANYA <75521183+104ananya@users.noreply.github.com>
2024-02-28 16:57:40 +0530

    Update the sorting section to display the total number of filtered products.

--- src/components/SortingSection.jsx
+++ src/components/SortingSection.jsx
@@ -3,22 +3,19 @@ import { BsFillGrid1X2Fill } from "react-icons/bs";
 import { useFilterContext } from "../context/filterContext";
 
 const SortingSection = () => {
-  const {setListView} = useFilterContext();
-
-
+  const { filter_products, setListView } = useFilterContext();
 
   return (
     <div className="flex justify-between mb-4">
       {/* Grid toggle  */}
       <div>
-        <button className="p-1.5 bg-gray-100" 
-        onClick={setListView}>
+        <button className="p-1.5 bg-gray-100" onClick={setListView}>
           <BsFillGrid1X2Fill />
         </button>
       </div>
 
       {/* Products available  */}
-      <div>Total</div>
+      <div>{filter_products.length} Total products</div>
 
       {/* Sorting function  */}
       <div>sort</div>
