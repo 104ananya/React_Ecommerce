5a3052f0a73f6a547edffd4712c672e2094549af
KUMARI ANANYA <75521183+104ananya@users.noreply.github.com>
2024-06-21 18:47:56 +0530

    Structured filterSection component

---
+++ src/components/CompanyFilter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CompanyFilter = () => {
+  return (
+    <div>CompanyFilter</div>
+  )
+}
+
+export default CompanyFilter
--- src/components/FilterSection.jsx
+++ src/components/FilterSection.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import SearchBar from './SearchBar';
+import CategoryFilter from './CategoryFilter';
+import CompanyFilter from './CompanyFilter';
+import ColorFilter from './ColorFilter';
+import PriceFilter from './PriceFilter';
 
 const FilterSection = () => {
   return (
-    <div>FilterSection</div>
-  )
-}
+    <div className="flex flex-col justify-around h-screen p-5">
+      <div className="p-2 border border-gray-300 rounded"><SearchBar /></div>
+      <div className="p-2 border border-gray-300 rounded"><CategoryFilter/></div>
+      <div className="p-2 border border-gray-300 rounded"><CompanyFilter/></div>
+      <div className="p-2 border border-gray-300 rounded"><ColorFilter/></div>
+      <div className="p-2 border border-gray-300 rounded"><PriceFilter/></div>
+      <div className="p-2 border border-gray-300 rounded">Item 6</div>
+    </div>
+  );
+};
 
-export default FilterSection
+export default FilterSection;
